d65dce75832862ded776e6686e5d3a7c5a98bc6f
shirin pandey <shirin.pandey@student.manchester.ac.uk>
2025-06-20 17:43:20 +0400

    New changes

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template,flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 import PyPDF2
+import os,nltk, json
 from io import BytesIO
-import os 
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 
 #referencing this file
@@ -19,35 +21,57 @@ def index():
 
 @app.route('/upload', methods = ['POST'])
 def upload():
-    #array of files we find cv 
     try:
-        file = request.files['cv'] 
-        extension = os.path.splitext(file.filename)[1]
-        if file:
-            if extension not in app.config['ALLOWED_EXTENSIONS']:
-                return "File should be pdf or txt"
-            if file.filename.endswith('.pdf'):
-                pdf_reader = PyPDF2.PdfReader(BytesIO(file.read()))
-                contents = ''
-                for page in pdf_reader.pages:
-                    contents += page.extract_text() or ""
-
-            else:
-                contents = file.read().decode('uti-8')
-            return render_template('results.html', cv_text = contents)
-
+        file = request.files['cv']
+        if not file:
+            return "No file uploaded"
+        contents = cv_reading(file)
+        new_file = storefiles()
+        clean_data(contents)
+        return render_template('results.html', job_listing = new_file)
     except RequestEntityTooLarge:
         return "File is larger than 16 MB"
-
+    
 
 def storefiles():
-    job = []
+    with open(os.path.join("jobs", "job.json"), 'r') as f:
+        jobs = json.load(f)
+        
 
-    for file in os.listdir("jobs"):
-        with open('filename.txt', 'r') as f:
-            content = f.read()
-            job.append(content)
-    return content
+    return jobs
+
+
+def clean_data(jobs):
+    lower_jobs = jobs.lower()
+    cleaned_char = []
+    for char in lower_jobs:
+        if char.isalnum() or char.isspace():
+            cleaned_char.append(char)
+    cleaned_data = ''.join(cleaned_char)
+    cleaned_no_space = ' '.join(cleaned_data.split())
+
+    stops = set(stopwords.words('english'))
+    word = cleaned_no_space.split()
+    filtered_words = [w for w in word if w not in stops]
+
+    return ' '.join(filtered_words)
+
+
+def cv_reading(file):
+    extension = os.path.splitext(file.filename)[1]
+    if file:
+        if extension not in app.config['ALLOWED_EXTENSIONS']:
+            return "File should be pdf or txt"
+        if file.filename.endswith('.pdf'):
+            pdf_reader = PyPDF2.PdfReader(BytesIO(file.read()))
+            contents = ''
+            for page in pdf_reader.pages:
+                contents += page.extract_text() or ""
+
+        else:
+            contents = file.read().decode('utf-8')
+    return contents
+
     
 if __name__ == "__main__":
     app.run(debug= True)
