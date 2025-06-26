897cfdd0eefe8b8075a8020c0623ad9238ea431b
shirin pandey <shirin.pandey@student.manchester.ac.uk>
2025-06-26 21:09:21 +0400

    logout done

--- auth.py
+++ auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template,request, flash
+from flask import Blueprint, render_template,request, flash, redirect, url_for
+from models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from extension import db 
+from flask_login import login_user,login_required, logout_user, current_user
 
 auth =  Blueprint('auth',__name__)
 
@@ -10,8 +14,10 @@ def signup():
         lastname = request.form.get('Lastname')
         password1 = request.form.get('Password')
         password2 = request.form.get('Password2')
-
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already signed in', category= 'error')
+        elif len(email) < 4:
             flash("Email must be greater than 4 charector", category='error')
         elif len(firstname) < 2 or len(lastname) < 2:
             flash("Name must be greater than 2 charector", category='error')
@@ -21,11 +27,36 @@ def signup():
         elif len(password1) < 7:
             flash("Password must be greater than 7 charector", category='error')
         else:
-            flash("Account created", category='success')
- 
+            new_user = User(email = email , firstname = firstname, lastname = lastname, password_hash = generate_password_hash(password1, method = 'pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
 
+            flash("Account created", category='success')
+        
     return render_template("signup.html")
 
 @auth.route('login', methods = ['GET','POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password_hash, password):
+                flash('Logged in succesfully', category='success')
+                login_user(user, remember=True)
+            else:
+                flash('Password isnt correct',category='error')
+        else:
+            flash('Email dont match',category='error')
+
+    return render_template("login.html", boolean = True)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
